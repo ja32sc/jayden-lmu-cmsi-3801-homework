9fe1ad6bf3df2170117b8ac580ae703da1fddf52
ja32sc <112520336+ja32sc@users.noreply.github.com>
2024-10-24 10:56:41 -0700

    Completed Exercises

--- haskell/Exercises.hs
+++ haskell/Exercises.hs
@@ -1,6 +1,17 @@
 module Exercises
     ( change,
-      -- put the proper exports here
+      firstThenApply,
+      powers,
+      meaningfulLineCount,
+      Shape(..),
+      volume,
+      surfaceArea,
+      is_approx,
+      BST(Empty),
+      size,
+      insert,
+      inorder,
+      contains
     ) where
 
 import qualified Data.Map as Map
@@ -20,12 +31,71 @@ change amount
                 (count, newRemaining) = remaining `divMod` d
                 newCounts = Map.insert d count counts
 
--- Write your first then apply function here
 
--- Write your infinite powers generator here
+firstThenApply :: [a] -> (a -> Bool) -> (a -> b) -> Maybe b
+firstThenApply xs pred f = fmap f (find pred xs)
 
--- Write your line count function here
 
--- Write your shape data type here
+powers :: Integral a => a -> [a]
+powers base = map (base^) [0..]
 
--- Write your binary search tree algebraic type here
+
+meaningfulLineCount :: FilePath -> IO Int
+meaningfulLineCount path = do
+    contents <- readFile path
+    return $ length $ filter meaningfulLine $ lines contents
+    where
+        meaningfulLine line = not (all isSpace line) && not ("--" `isPrefixOf` line)
+
+
+data Shape
+    = Sphere Double
+    | Box Double Double Double
+    deriving (Eq, Show)
+
+volume :: Shape -> Double
+volume (Sphere r) = (4 / 3) * pi * r^3
+volume (Box l w h) = l * w * h
+
+surfaceArea :: Shape -> Double
+surfaceArea (Sphere r) = 4 * pi * r^2
+surfaceArea (Box l w h) = 2 * (l * w + l * h + w * h)
+
+is_approx :: Double -> Double -> Bool
+x `is_approx` y = abs (x - y) < 0.0000001
+
+
+data BST a
+    = Empty
+    | Node a (BST a) (BST a)
+
+size :: BST a -> Int
+size Empty = 0
+size (Node _ left right) = 1 + size left + size right
+
+inorder :: BST a -> [a]
+inorder Empty = []
+inorder (Node value left right) = inorder left ++ [value] ++ inorder right
+
+insert :: Ord a => a -> BST a -> BST a
+insert value Empty = Node value Empty Empty
+insert value (Node nodeValue left right)
+    | value < nodeValue = Node nodeValue (insert value left) right
+    | value > nodeValue = Node nodeValue left (insert value right)
+    | otherwise = Node nodeValue left right
+
+instance (Show a) => Show (BST a) where
+    show :: Show a => BST a -> String
+    show Empty = "()"
+    show (Node value Empty Empty) = "(" ++ show value ++ ")"
+    show (Node value left Empty) = "(" ++ show left ++ show value ++ ")"
+    show (Node value Empty right) = "(" ++ show value ++ show right ++ ")"
+    show (Node value left right) = "(" ++ show left ++ show value ++ show right ++ ")"
+
+
+contains :: Ord a => a -> BST a -> Bool
+contains _ Empty = False
+contains value (Node nodeValue left right)
+    | value < nodeValue = contains value left
+    | value > nodeValue = contains value right
+    | otherwise = True
